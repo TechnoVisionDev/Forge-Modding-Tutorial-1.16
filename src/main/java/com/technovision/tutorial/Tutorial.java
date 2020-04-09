cdb7c91668f54f9892d5b7f21b4572253891ede5
TechnoVisionDev <thomasanthonypeters@gmail.com>
2020-04-09 16:21:29 -0700

    Add custom creative tab

--- src/main/java/com/technovision/tutorial/Tutorial.java
+++ src/main/java/com/technovision/tutorial/Tutorial.java
@@ -1,6 +1,10 @@
 package com.technovision.tutorial;
 
 import com.technovision.tutorial.util.RegistryHandler;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -27,4 +31,13 @@ public class Tutorial
     private void setup(final FMLCommonSetupEvent event) { }
 
     private void doClientStuff(final FMLClientSetupEvent event) { }
+
+    // Custom ItemGroup TAB
+    public static final ItemGroup TAB = new ItemGroup("tutorialTab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(RegistryHandler.RUBY.get());
+        }
+    };
+
 }
--- src/main/java/com/technovision/tutorial/items/ItemBase.java
+++ src/main/java/com/technovision/tutorial/items/ItemBase.java
@@ -1,11 +1,12 @@
 package com.technovision.tutorial.items;
 
+import com.technovision.tutorial.Tutorial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class ItemBase extends Item {
 
     public ItemBase() {
-        super(new Item.Properties().group(ItemGroup.MATERIALS));
+        super(new Item.Properties().group(Tutorial.TAB));
     }
 }
