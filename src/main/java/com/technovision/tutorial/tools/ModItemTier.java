e34e51e48c326a61a9b110f7a3406b1c6c993978
TechnoVisionDev <thomasanthonypeters@gmail.com>
2020-07-05 19:15:32 -0700

    Custom Tools

---
+++ src/main/java/com/technovision/tutorial/tools/ModItemTier.java
@@ -0,0 +1,60 @@
+package com.technovision.tutorial.tools;
+
+import com.technovision.tutorial.util.RegistryHandler;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+
+    RUBY(2, 800, 7.0F, 3.0F, 12, () -> {
+        return Ingredient.fromItems(RegistryHandler.RUBY_SWORD.get());
+    });
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairMaterial;
+
+    ModItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.get();
+    }
+}
--- src/main/java/com/technovision/tutorial/util/RegistryHandler.java
+++ src/main/java/com/technovision/tutorial/util/RegistryHandler.java
@@ -4,8 +4,9 @@ import com.technovision.tutorial.Tutorial;
 import com.technovision.tutorial.blocks.BlockItemBase;
 import com.technovision.tutorial.blocks.RubyBlock;
 import com.technovision.tutorial.items.ItemBase;
+import com.technovision.tutorial.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -23,6 +24,22 @@ public class RegistryHandler {
 
     // Items
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+    // Tools
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
+            new SwordItem(ModItemTier.RUBY, 2, -2.4F, new Item.Properties().group(Tutorial.TAB)));
+
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () ->
+            new PickaxeItem(ModItemTier.RUBY, 0, -2.8F, new Item.Properties().group(Tutorial.TAB)));
+
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () ->
+            new ShovelItem(ModItemTier.RUBY,0.5F, -3.0F, new Item.Properties().group(Tutorial.TAB)));
+
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () ->
+            new AxeItem(ModItemTier.RUBY, 5, -3.1F, new Item.Properties().group(Tutorial.TAB)));
+
+    public static final RegistryObject<HoeItem> RUBY_HOE = ITEMS.register("ruby_hoe", () ->
+            new HoeItem(ModItemTier.RUBY, -1.0F, new Item.Properties().group(Tutorial.TAB)));
 
     // Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
